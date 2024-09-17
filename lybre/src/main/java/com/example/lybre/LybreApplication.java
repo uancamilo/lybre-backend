96c4984d59921451b6bcc3e8c57c6300b8251974
Juan Camilo <uancamilo@gmail.com>
2024-09-17 17:32:07 -0500

    Primer commit

---
+++ lybre/src/main/java/com/example/lybre/LybreApplication.java
@@ -0,0 +1,13 @@
+package com.example.lybre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LybreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LybreApplication.class, args);
+	}
+
+}
---
+++ lybre/src/test/java/com/example/lybre/LybreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lybre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LybreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
